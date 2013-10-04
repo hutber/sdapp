4bbd0aeea90744654f87831fcfcbc3088aab2261
Jamie Hutber (Home) <jamie@hutber.com>
2013-10-04 16:26:17 +0200

    asd

--- docroot/js/main.js
+++ docroot/js/main.js
@@ -1,5 +1,8 @@
 /*global require*/
+
+alert('pop -1');
 'use strict';
+alert('pop 0');
 //Because I am lazy I rebind console to c
 var c = false; if(typeof console === "object" && typeof console.error === "function"){ c = function (msg){console.info(msg);}; }else{ c =  function (msg){alert(msg);};}
 window.isphone = false; if(document.URL.indexOf("local") > 0 || document.URL.indexOf("sex") > 0) {	window.isphone = true;}
@@ -52,6 +55,7 @@ require.config({
     }
 });
 
+alert('pop');
 //Start off the router
 require([
     // Requires ----------------
@@ -76,12 +80,13 @@ require([
 	'views/sex/anything'
 
 ], function () {
+	alert('pop 2');
 	//Check to see if we are in the live app
-	if(!window.isphone){
-		$.getScript('cordova.js', function() { alert('Load was performed.'); });
-	}else{
-		$.getScript('http://localhost:35729/livereload.js', function() { console.info('Load was performed.'); });
-	}
+//	if(!window.isphone){
+//		$.getScript('cordova.js', function() { alert('Load was performed.'); });
+//	}else{
+//		$.getScript('http://localhost:35729/livereload.js', function() { console.info('Load was performed.'); });
+//	}
 
     //set arguments to values for ease of reading arguments
     var Backbone = arguments[0],
@@ -96,47 +101,47 @@ require([
         anythingView = arguments[11];
 
 	SD.ARGS = arguments;
-
-    // initiate routers ----------------
-    var router = new Router();
-
-    // views ---------------------------
-    var homeView = new HomeView();
-    var loginView = new LoginView();
-
-	// Sex views ---------------------------
-	var WankView = new wankView();
-	var FingersView = new fingersView();
-	var OralView = new oralView();
-	var SexView = new sexView();
-	var AnythingView = new anythingView();
-
-	// Router ---------------------------
-	router.on('route:login', function(){
-		if(sessionStorage.getItem('privateKey')!==null){
-			homeView.render();
-		}else{
-//			SD.checkConnection();
-			loginView.render();
-		}
-	});
-
-	// Sex Routers ---------------------------
-	router.on('route:wank', function(){
-		WankView.render();
-	});
-	router.on('route:fingers', function(){
-		FingersView.render();
-	});
-	router.on('route:oral', function(){
-		OralView.render();
-	});
-	router.on('route:sex', function(){
-		SexView.render();
-	});
-	router.on('route:anything', function(){
-		AnythingView.render();
-	});
+//
+////			SD.checkConnectio
+//			// initiate routers ----------------
+//			var router = new Router();
+//
+//			// views ---------------------------
+//			var homeView = new HomeView();
+//			var loginView = new LoginView();
+//
+//			// Sex views ---------------------------
+//			var WankView = new wankView();
+//			var FingersView = new fingersView();
+//			var OralView = new oralView();
+//			var SexView = new sexView();
+//			var AnythingView = new anythingView();
+//
+//			// Router ---------------------------
+//			router.on('route:login', function(){
+//				if(sessionStorage.getItem('privateKey')!==null){
+//					homeView.render();
+//				}else{n();
+//			loginView.render();
+//		}
+//	});
+//
+//	// Sex Routers ---------------------------
+//	router.on('route:wank', function(){
+//		WankView.render();
+//	});
+//	router.on('route:fingers', function(){
+//		FingersView.render();
+//	});
+//	router.on('route:oral', function(){
+//		OralView.render();
+//	});
+//	router.on('route:sex', function(){
+//		SexView.render();
+//	});
+//	router.on('route:anything', function(){
+//		AnythingView.render();
+//	});
 	document.addEventListener("deviceready", Backbone.history.start, false);
 
 	$(document).ready(function() {
@@ -144,5 +149,5 @@ require([
 		Backbone.history.start();
 	});
 
-	SD.centerItems($('content'));
+//	SD.centerItems($('content'));
 });
