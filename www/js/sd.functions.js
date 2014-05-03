f66a8cd09f769ce94eeb2d4d4fbf761691810537
Jamie Hutber - Home <jamie@hutber.com>
2014-05-04 01:54:47 +0200

    as

--- www/js/sd.functions.js
+++ www/js/sd.functions.js
@@ -220,23 +220,21 @@ SD.changeHeightofContent = function(){
 
 SD.onHashChange = function(){
 	//make sure we are logged in, if we are not forward back to home page
-
-	alert('2');
 	SD.login.checkLoginState();
-	alert('3');
-	alert(SD.HASH);
+
 	//Updated previous hash
 	SD.PREVIOUSHASH = SD.HASH;
-	alert('4');
+
 	//Update the new hash
 	SD.HASH = window.location.hash.substring(1);
-	alert('5');
+	alert(SD.HASH);
+
 	//On page load update body class with current page
 	SD.DV.globalClass();
-	alert('6');
+
 	//Resize the $('page') element
 	SD.changeHeightofContent();
-	alert('7');
+
 	//update menu items with selected item
 	$('menu a.selected').removeAttr('class');
 	$('menu a[data-id='+SD.HASH+']').addClass('selected');
--- www/js/views/homeView.js
+++ www/js/views/homeView.js
@@ -1,7 +1,7 @@
 define([
 	'sd',
 	'dv',
-	'slider',
+//	'slider',
 ], function () {
     'use strict';
 
@@ -13,48 +13,48 @@ define([
 		],
         render: function () {
 			SD.setTitle('SELECT SOME SEXYNESS');
-			this.$el.html(this.template);
+//			this.$el.html(this.template);
 
-			SD.SLIDER = $('.royalSlider').royalSlider({ //Set up slider
-				controlNavigation: 'none',
-				arrowsNavHideOnTouch: true,
-				globalCaption: true,
-				globalCaptionInside: true,
-				imageScaleMode: 'fit',
-				arrowsNav: false,
-				thumbs: {
-					arrows: false,
-					appendSpan: false,
-					firstMargin: false,
-					autoCenter: false,
-					spacing: 5
-				}
-			}).data('royalSlider');
-
-			//if we have the sex nav open on load select the correct class
-			if(SD.SEXDEFAULTS.sextype === "default"){
-				$('div[data-type=wank]').addClass('selected');
-			} else{
-				//Now navigate to current selection
-				SD.SLIDER.goTo($('sexnav div.selected').index());
-			}
-
-			SD.SLIDER.ev.on('rsSlideClick', function() { //Add click events to the sex icons
-				SD.pageLoad($('.rsGCaption').find('anchor').attr('id'));
-			});
-
-			SD.SLIDER.ev.on('rsAfterSlideChange', function(event) {
-				if($('.royalSlider')[0]){
-					//make sure no elements have any selected items
-					$('sexnav div').removeClass('selected');
-
-					//Work out current ID
-					var currentSex = $(SD.SLIDER.currSlide.caption).attr('id');
-
-					//update current sex with the class
-					$('div[data-type='+currentSex+']').addClass('selected');
-				}
-			});
+//			SD.SLIDER = $('.royalSlider').royalSlider({ //Set up slider
+//				controlNavigation: 'none',
+//				arrowsNavHideOnTouch: true,
+//				globalCaption: true,
+//				globalCaptionInside: true,
+//				imageScaleMode: 'fit',
+//				arrowsNav: false,
+//				thumbs: {
+//					arrows: false,
+//					appendSpan: false,
+//					firstMargin: false,
+//					autoCenter: false,
+//					spacing: 5
+//				}
+//			}).data('royalSlider');
+//
+//			//if we have the sex nav open on load select the correct class
+//			if(SD.SEXDEFAULTS.sextype === "default"){
+//				$('div[data-type=wank]').addClass('selected');
+//			} else{
+//				//Now navigate to current selection
+//				SD.SLIDER.goTo($('sexnav div.selected').index());
+//			}
+//
+//			SD.SLIDER.ev.on('rsSlideClick', function() { //Add click events to the sex icons
+//				SD.pageLoad($('.rsGCaption').find('anchor').attr('id'));
+//			});
+//
+//			SD.SLIDER.ev.on('rsAfterSlideChange', function(event) {
+//				if($('.royalSlider')[0]){
+//					//make sure no elements have any selected items
+//					$('sexnav div').removeClass('selected');
+//
+//					//Work out current ID
+//					var currentSex = $(SD.SLIDER.currSlide.caption).attr('id');
+//
+//					//update current sex with the class
+//					$('div[data-type='+currentSex+']').addClass('selected');
+//				}
+//			});
 
         }
     });
