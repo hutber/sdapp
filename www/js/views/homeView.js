1b046a2767f306b14790fe2e123f668b3777ec65
Jamie Hutber - Home <jamie@hutber.com>
2014-05-10 00:46:05 +0200

    buggy bug

--- www/js/views/homeView.js
+++ www/js/views/homeView.js
@@ -1,7 +1,7 @@
 define([
 	'sd',
 	'dv',
-	'slider'
+//	'slider'
 ], function () {
     'use strict';
 
@@ -18,48 +18,48 @@ define([
 			SD.setTitle('SELECT SOME SEXYNESS');
 			this.$el.html(this.template);
 
-			if(jQuery.fn.royalSlider){
-				SD.SLIDER = $('.royalSlider').royalSlider({ //Set up slider
-					controlNavigation: 'none',
-					arrowsNavHideOnTouch: true,
-					globalCaption: true,
-					globalCaptionInside: true,
-					imageScaleMode: 'fit',
-					arrowsNav: false,
-					thumbs: {
-						arrows: false,
-						appendSpan: false,
-						firstMargin: false,
-						autoCenter: false,
-						spacing: 5
-					}
-				}).data('royalSlider');
-
-				//if we have the sex nav open on load select the correct class
-				if(SD.SEXDEFAULTS.sextype === "default"){
-					$('div[data-type=wank]').addClass('selected');
-				} else{
-					//Now navigate to current selection
-					SD.SLIDER.goTo($('sexnav div.selected').index());
-				}
-
-				SD.SLIDER.ev.on('rsSlideClick', function() { //Add click events to the sex icons
-					SD.pageLoad($('.rsGCaption').find('anchor').attr('id'));
-				});
-
-				SD.SLIDER.ev.on('rsAfterSlideChange', function(event) {
-					if($('.royalSlider')[0]){
-						//make sure no elements have any selected items
-						$('sexnav div').removeClass('selected');
-
-						//Work out current ID
-						var currentSex = $(SD.SLIDER.currSlide.caption).attr('id');
-
-						//update current sex with the class
-						$('div[data-type='+currentSex+']').addClass('selected');
-					}
-				});
-			}else{
+//			if(jQuery.fn.royalSlider){
+//				SD.SLIDER = $('.royalSlider').royalSlider({ //Set up slider
+//					controlNavigation: 'none',
+//					arrowsNavHideOnTouch: true,
+//					globalCaption: true,
+//					globalCaptionInside: true,
+//					imageScaleMode: 'fit',
+//					arrowsNav: false,
+//					thumbs: {
+//						arrows: false,
+//						appendSpan: false,
+//						firstMargin: false,
+//						autoCenter: false,
+//						spacing: 5
+//					}
+//				}).data('royalSlider');
+//
+//				//if we have the sex nav open on load select the correct class
+//				if(SD.SEXDEFAULTS.sextype === "default"){
+//					$('div[data-type=wank]').addClass('selected');
+//				} else{
+//					//Now navigate to current selection
+//					SD.SLIDER.goTo($('sexnav div.selected').index());
+//				}
+//
+//				SD.SLIDER.ev.on('rsSlideClick', function() { //Add click events to the sex icons
+//					SD.pageLoad($('.rsGCaption').find('anchor').attr('id'));
+//				});
+//
+//				SD.SLIDER.ev.on('rsAfterSlideChange', function(event) {
+//					if($('.royalSlider')[0]){
+//						//make sure no elements have any selected items
+//						$('sexnav div').removeClass('selected');
+//
+//						//Work out current ID
+//						var currentSex = $(SD.SLIDER.currSlide.caption).attr('id');
+//
+//						//update current sex with the class
+//						$('div[data-type='+currentSex+']').addClass('selected');
+//					}
+//				});
+//			}else{
 				$('.royalSlider a').each(function(){
 					var type = $(this)[0].id,
 						copiedContent = $(this).find('anchor').clone(),
@@ -74,7 +74,7 @@ define([
 					//Create link
 					$('.royalSlider').append(newLink);
 				});
-			}
+//			}
         }
     });
     return HomeView;
