793e9ec763081a96549c77e0542ce432d8ef06b6
Jamie Hutber - Home <jamie@hutber.com>
2014-07-01 19:05:58 +0200

    as

--- www/js/views/homeView.js
+++ www/js/views/homeView.js
@@ -58,7 +58,7 @@ define([
 
 				SD.SLIDER.ev.on('rsAfterSlideChange', function(event) {
 //					make sure no elements have any selected items
-					$('sexnav div.selected').removeClass('class');
+					$('sexnav div.selected').removeClass('selected');
 					$('sexnav div').eq(SD.SLIDER.currSlideId).addClass('selected');
 					//update current sex with the class
 //					$('div[data-type='+currentSex+']').addClass('selected');
