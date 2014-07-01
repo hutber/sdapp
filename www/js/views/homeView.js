74fe2609ac7dcb2fce317aff5cc949cd00574912
Jamie Hutber - Home <jamie@hutber.com>
2014-07-01 19:19:44 +0200

    as

--- www/js/views/homeView.js
+++ www/js/views/homeView.js
@@ -58,8 +58,8 @@ define([
 
 				SD.SLIDER.ev.on('rsAfterSlideChange', function(event) {
 //					make sure no elements have any selected items
-					$('sexnav div.selected').removeClass('selected');
-					$('sexnav div').eq(SD.SLIDER.currSlideId).addClass('selected');
+					$('sexnav div.selected')[0].removeAttribute('class');
+//					$('sexnav div').eq(SD.SLIDER.currSlideId).addClass('selected');
 					//update current sex with the class
 //					$('div[data-type='+currentSex+']').addClass('selected');
 				});
