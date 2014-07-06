7fc1d7a4cdf6ab4c04c74ce86401403da7ffcd0f
Jamie Hutber - Home <jamie@hutber.com>
2014-07-06 11:50:51 +0200

    as

--- www/js/views/whos/managewhos.js
+++ www/js/views/whos/managewhos.js
@@ -9,7 +9,7 @@ define([
 		template: JST['templates/whos/managewhos.ejs'],
 		removeWho: function(me){
 			var parentMe = $(me.currentTarget).parent(), whoName = parentMe.data('name'), whoId = parentMe.data('id');
-			SD.UI.Dialog('Delete Who?', 'Do you really want to delete ' + whoName, ['Yes Please', 'Cancel'], function(){
+			SD.UI.Dialog('Delete Who?', 'Do you really want to delete ' + whoName, ['Cancel', 'Yes Please'], function(){
 				c(arguments);
 				SD.spinner.show();
 				$.ajax({
